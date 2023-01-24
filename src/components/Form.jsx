76537e5baad6ab2723517dcf1d259b475efc4ad9
AndriiGrytsenkoVH <andrew.grit.vh@gmail.com>
2023-01-24 11:06:56 -0600

    omg it works

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,18 +13,16 @@ class Form extends React.Component{
 	}
 
 	handleSubmit(event) {
-		this.handleAddClick(this.state.value);
-		event.preventDefault();
+		this.props.handleAddClick(this.state.value);
+		// event.preventDefault();
 	}
 
   	render() {
 		return(
-			<form onSubmit={this.handleSubmit}>
-				<div className="input-group mb-3">
-					<button className="btn btn-outline-secondary" type="submit" id={this.props.id}>Add</button>
-					<input type="text" className="form-control" placeholder="To-Do" aria-label="To-Do" aria-describedby={this. props.id} onChange={ this.handleChange }/>
-				</div>
-  			</form>
+			<div className="input-group mb-3">
+				<button type="button" className="btn btn-outline-secondary" id={this.props.id} onClick={this.handleSubmit}>Add</button>
+				<input type="text" className="form-control" placeholder="To-Do" aria-label="To-Do" aria-describedby={this. props.id} onChange={ this.handleChange }/>
+			</div>
 		)
 	}
 }
